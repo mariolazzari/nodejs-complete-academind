f15a3a9547697dfc3ab027caab06d3c4e0496e64
Mario Lazzari <mario.lazzari@gmail.com>
2019-06-01 17:35:58 +0200

    Done lesson 30 chapter 3

--- ch03_basics/app.js
+++ ch03_basics/app.js
@@ -1,7 +1,14 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  const { url, method, headers } = req;
+  console.log("req url", url);
+  console.log("req method", method);
+  console.log("req headers", headers);
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<h1>Ciao</h1>");
+  res.end();
 });
 
 server.listen(3000);
