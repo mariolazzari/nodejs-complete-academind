4fa7a9d24e8c4e82cc8eeabfe648f22379101499
Mario Lazzari <mario.lazzari@gmail.com>
2019-06-02 10:08:04 +0200

    Done lesson 33 chapter 3

--- ch03_basics/app.js
+++ ch03_basics/app.js
@@ -1,14 +1,25 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const { url, method, headers } = req;
+
   console.log("req url", url);
   console.log("req method", method);
   console.log("req headers", headers);
 
-  res.setHeader("Content-Type", "text/html");
-  res.write("<h1>Ciao</h1>");
-  res.end();
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<h1>Home page</h1>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "ciao");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
 });
 
 server.listen(3000);
